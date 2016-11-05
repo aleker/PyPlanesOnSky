5d91cc5935fd5b590f5eee4bbd517649a3230feb
aleker <aleker@users.noreply.github.com>
2016-11-05 21:39:32 +0100

    Not ready yet.

--- parameters.py
+++ parameters.py
@@ -4,7 +4,7 @@ dil_ero_range = 1
 low = 95
 heigh = 100
 # in canny
-sigma = 3
+sigma = 4	# or 5
 
 # change name of files (add name at the beginning)
 name = ''
--- plane.py
+++ plane.py
@@ -6,6 +6,7 @@ from skimage import morphology
 from skimage.filters import roberts, sobel, scharr, prewitt
 from skimage import feature
 from skimage import exposure
+from skimage import filters
 import os
 import numpy as np
 from matplotlib import pyplot as plt
@@ -14,65 +15,52 @@ import parameters
 
 
 def filter(image):
-
+	image = filters.gaussian (image, sigma=0.4)
 	for i in range(parameters.dil_ero_range):
 		working_image = image
 		image = morphology.dilation (working_image)
 		working_image = image
 		image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
 
-	p_low, p_heigh = np.percentile (image, (0, 50))
+	# Contrast stretching (smaller range on black)
+	p_low, p_heigh = np.percentile (image, (0, 40))		#0, 40
 	image = exposure.rescale_intensity (image, in_range=(p_low, p_heigh))
 
-	image = roberts (image)
-	#image = sobel (image)
-	# image = feature.canny(image, parameters.sigma)
-
-	p_low, p_heigh = np.percentile (image, (98, 100))
-	image = exposure.rescale_intensity(image, in_range=(p_low, p_heigh))
-	#image = exposure.equalize_hist (image)
-	image = exposure.equalize_adapthist(image, clip_limit=0.7)
-	for i in range(3):
-		working_image = image
-		image = morphology.dilation (working_image)
-		working_image = image
-		image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges
+	
+	# # # DETECT EDGES:
+	# # image = roberts (image)
+	# #image = sobel (image)
+	# image = feature.canny(image, 6)	# 5
+	#
+	# # Contrast stretching (smaller range on white)
+	# # p_low, p_heigh = np.percentile (image, (90, 100))	#98, 100
+	# # image = exposure.rescale_intensity(image, in_range=(p_low, p_heigh))
+	#
+	# # Adaptive Equalization
+	# image = exposure.equalize_adapthist(image, clip_limit=0.1) # 0.07
 
 	return image
 
 
-def contour_filter(image):
-	for i in range(parameters.dil_ero_range):
-		working_image = image
-		image = morphology.dilation (working_image)
-		working_image = image
-		image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
+def contour_filter(image, original_image):
+	# Contours
+	contours = measure.find_contours(image, 0.95, fully_connected='high')
+	print(len(contours), '\n')
+	figure = plt
+	fig, ax = figure.subplots()
+	ax.imshow (original_image, interpolation='nearest', cmap=plt.cm.gray)
+	for n, contour in enumerate (contours):
+		ax.plot (contour[:, 1], contour[:, 0], linewidth=2)
+	ax.axis ('off')
+	return figure
 
-	p_low, p_heigh = np.percentile (image, (0, 50))
-	image = exposure.rescale_intensity (image, in_range=(p_low, p_heigh))
 
-	#image = exposure.equalize_adapthist(image, clip_limit=0.7)
-
-	# # Contours
-	# contours = measure.find_contours(image, 0.3)
-	# figure = plt
-	# fig, ax = figure.subplots ()
-	# #ax.imshow (image, interpolation='nearest', cmap=plt.cm.gray)
-	# for n, contour in enumerate (contours):
-	# 	ax.plot (contour[:, 1], contour[:, 0], linewidth=2)
-	# ax.set_xticks ([])
-	# ax.set_yticks ([])
-	# figure.savefig(image)
-	return image
-
-def separate_figure (file_name):
+def file_processing (file_name):
 	filename = os.path.join (os.getcwd (), file_name)
 	original_image = io.imread (filename)
 	image = io.imread (filename, as_grey=True)
-	image_1 = filter(image)
-	image_2 = contour_filter(image)
-	image = image_2
 
+	image = filter(image)
 	# SAVING OUTPUT:
 	output_path = os.path.join(os.getcwd(), "output/")
 	if not os.path.exists(output_path):
@@ -80,12 +68,20 @@ def separate_figure (file_name):
 	new_path = os.path.join(output_path, os.path.basename(file_name) + parameters.name)
 	io.imsave(new_path, image)
 
+	# image = contour_filter(image, original_image)
+	# # SAVING OUTPUT:
+	# output_path = os.path.join(os.getcwd(), "output_2/")
+	# if not os.path.exists(output_path):
+	# 	os.mkdir(output_path)
+	# new_path = os.path.join(output_path, os.path.basename(file_name) + parameters.name)
+	# image.savefig(new_path)
+
 
 def read_files ():
 	files_list = glob.glob ("planes/*.jpg")
 	for file in files_list:
-		separate_figure(file)
-	#separate_figure("planes/samolot04.jpg")
+		file_processing(file)
+	#file_processing("planes/samolot04.jpg")
 
 
 
