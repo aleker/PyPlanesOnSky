7cd8bb33dc9a682d315d722df569df3a87296ed0
aleker <aleker@users.noreply.github.com>
2016-11-07 16:13:34 +0100

    For 3 version

--- parameters.py
+++ parameters.py
@@ -1,10 +1,2 @@
-# range of dilation and erosion
-dil_ero_range = 1
-# percentile (in rescale_intensity)
-low = 95
-heigh = 100
-# in canny
-sigma = 4	# or 5
-
 # change name of files (add name at the beginning)
 name = ''
--- plane.py
+++ plane.py
@@ -15,74 +15,43 @@ import parameters
 
 
 def filter(image):
+	# GAUSSIAN
 	image = filters.gaussian (image, sigma=0.4)
-	for i in range(parameters.dil_ero_range):
-		working_image = image
-		image = morphology.dilation (working_image)
-		working_image = image
-		image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
 
-	# Contrast stretching (smaller range on black)
-	p_low, p_heigh = np.percentile (image, (0, 40))		#0, 40
-	image = exposure.rescale_intensity (image, in_range=(p_low, p_heigh))
+	# EROSION
+	working_image = image
+	image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
 
-	
-	# # # DETECT EDGES:
-	# # image = roberts (image)
-	# #image = sobel (image)
-	# image = feature.canny(image, 6)	# 5
-	#
-	# # Contrast stretching (smaller range on white)
-	# # p_low, p_heigh = np.percentile (image, (90, 100))	#98, 100
-	# # image = exposure.rescale_intensity(image, in_range=(p_low, p_heigh))
-	#
-	# # Adaptive Equalization
-	# image = exposure.equalize_adapthist(image, clip_limit=0.1) # 0.07
+	# CANNY
+	image = feature.canny(image, sigma=4)
+
+	# DILATION
+	working_image = image
+	image = morphology.dilation (working_image)
+
+	# Adaptive Equalization
+	image = exposure.equalize_adapthist(image, clip_limit=0.1) 	# after canny
 
 	return image
 
 
-def contour_filter(image, original_image):
-	# Contours
-	contours = measure.find_contours(image, 0.95, fully_connected='high')
-	print(len(contours), '\n')
-	figure = plt
-	fig, ax = figure.subplots()
-	ax.imshow (original_image, interpolation='nearest', cmap=plt.cm.gray)
-	for n, contour in enumerate (contours):
-		ax.plot (contour[:, 1], contour[:, 0], linewidth=2)
-	ax.axis ('off')
-	return figure
-
-
 def file_processing (file_name):
 	filename = os.path.join (os.getcwd (), file_name)
-	original_image = io.imread (filename)
 	image = io.imread (filename, as_grey=True)
 
 	image = filter(image)
 	# SAVING OUTPUT:
-	output_path = os.path.join(os.getcwd(), "output/")
+	output_path = os.path.join(os.getcwd(), "output_for3/")
 	if not os.path.exists(output_path):
 		os.mkdir(output_path)
 	new_path = os.path.join(output_path, os.path.basename(file_name) + parameters.name)
 	io.imsave(new_path, image)
-
-	# image = contour_filter(image, original_image)
-	# # SAVING OUTPUT:
-	# output_path = os.path.join(os.getcwd(), "output_2/")
-	# if not os.path.exists(output_path):
-	# 	os.mkdir(output_path)
-	# new_path = os.path.join(output_path, os.path.basename(file_name) + parameters.name)
-	# image.savefig(new_path)
 
 
 def read_files ():
 	files_list = glob.glob ("planes/*.jpg")
 	for file in files_list:
 		file_processing(file)
-	#file_processing("planes/samolot04.jpg")
-
 
 
 if __name__ == '__main__':
