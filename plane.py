84e71e0b53557865c81da0f634a4a78e1a882145
aleker <aleker@users.noreply.github.com>
2016-11-08 13:22:10 +0100

    Merged two versions (for_3 and for_5).

--- plane.py
+++ plane.py
@@ -14,36 +14,6 @@ import glob
 import parameters
 
 
-def filter(image):
-	# GAUSSIAN
-	image = filters.gaussian (image, sigma=0.4)
-
-	# EROSION
-	working_image = image
-	image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
-
-	# DILATION
-	# working_image = image
-	# image = morphology.dilation (working_image)
-
-	# Contrast stretching (smaller range on black)
-	# im mniejszy range tym bardziej gubią się w chmurach
-	p_low, p_heigh = np.percentile (image, (0, 30))  # 0, 40
-	image = exposure.rescale_intensity (image, in_range=(p_low, p_heigh))
-
-	# # DETECT EDGES:
-	# image = feature.canny(image, 4)	# 5
-	#
-	# # DILATION
-	# working_image = image
-	# image = morphology.dilation (working_image)
-	#
-	# # Adaptive Equalization
-	# image = exposure.equalize_adapthist(image, clip_limit=0.8) # after canny 0.07
-
-	return image
-
-
 def is_close(beginning, ending):
 	boundary = 30
 	if beginning[0] > ending[0] and (beginning[0] > (ending[0] + boundary)):
@@ -58,11 +28,48 @@ def is_close(beginning, ending):
 		return True
 
 
+def filter(image):
+	# GAUSSIAN
+	image = filters.gaussian (image, sigma=0.4)
+
+	# EROSION
+	working_image = image
+	image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
+
+	# DETECT EDGES (CANNY)
+	image = feature.canny (image, sigma=4)
+
+	# DILATION
+	working_image = image
+	image = morphology.dilation (working_image)
+
+	# Adaptive Equalization
+	image = exposure.equalize_adapthist (image, clip_limit=0.1)  # after canny
+
+	return image
+
+
 def contour_filter(image, original_image):
+	# GAUSSIAN
+	image = filters.gaussian (image, sigma=0.5)
+
+	# EROSION
+	working_image = image
+	image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
+
+	# Contrast stretching (smaller range on black)
+	# smaller range - they lose in clouds
+	p_low, p_heigh = np.percentile (image, (0, 30))  # 0, 40
+	image = exposure.rescale_intensity (image, in_range=(p_low, p_heigh))
+
+	# EROSION x2
+	for i in range(2):
+		working_image = image
+		image = morphology.erosion (working_image)  # Erosion shrinks bright regions and enlarges dark regions
+
 	# CONTOURS
-	#contours = measure.find_contours(image, 0.8, fully_connected='high')
 	my_contours = []
-	# im wyższe tym bardziej reaguje na jasne (robią się pojedyncze krawedzie ale czasem promienie wchodza na samolot
+	# if higher than there is more reaction on brighter pixels (but rows tuck in plane)
 	for contour in measure.find_contours(image, 0.5, fully_connected='high'):
 		if len(contour) > 400 and (is_close(contour[0], contour[-1])) is True:
 			my_contours.append(contour)
@@ -71,7 +78,7 @@ def contour_filter(image, original_image):
 	# PLOT
 	figure = plt
 	fig, ax = figure.subplots()
-	ax.imshow (image, interpolation='nearest', cmap=plt.cm.gray)
+	ax.imshow (original_image, interpolation='nearest', cmap=plt.cm.gray)
 	for n, contour in enumerate (my_contours):
 		ax.plot (contour[:, 1], contour[:, 0], linewidth=2)
 	ax.axis ('off')
@@ -82,18 +89,21 @@ def file_processing (file_name):
 	filename = os.path.join (os.getcwd (), file_name)
 	original_image = io.imread (filename)
 	image = io.imread (filename, as_grey=True)
+	working_image = image	# this one will be unchanged
 
-	image = filter(image)
+	# 1) excercise for 3
+	image = filter(working_image)
 	# SAVING OUTPUT:
-	output_path = os.path.join(os.getcwd(), "output/")
+	output_path = os.path.join(os.getcwd(), "output_3/")
 	if not os.path.exists(output_path):
 		os.mkdir(output_path)
 	new_path = os.path.join(output_path, os.path.basename(file_name) + parameters.name)
 	io.imsave(new_path, image)
 
-	image = contour_filter(image, original_image)
+	# 2) exercise for 5
+	image = contour_filter(working_image, original_image)
 	# SAVING OUTPUT:
-	output_path = os.path.join(os.getcwd(), "output_2/")
+	output_path = os.path.join(os.getcwd(), "output_5/")
 	if not os.path.exists(output_path):
 		os.mkdir(output_path)
 	new_path = os.path.join(output_path, os.path.basename(file_name) + parameters.name)
@@ -104,8 +114,6 @@ def read_files ():
 	files_list = glob.glob ("planes/*.jpg")
 	for file in files_list:
 		file_processing(file)
-	#file_processing("planes/samolot04.jpg")
-
 
 
 if __name__ == '__main__':
